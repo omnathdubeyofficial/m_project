ec1a0deafdea6bb38c9f99886d1eccbd4b8eb454
rvp0001 <34305593+rvp0001@users.noreply.github.com>
2025-03-10 16:40:54 +0530

    upload

--- src/app/Home/School_Landing_Page/Layout/Header/index.tsx
+++ src/app/Home/School_Landing_Page/Layout/Header/index.tsx
@@ -129,12 +129,7 @@ useEffect(() => {
           >
             Login
           </Link>
-          <Link
-            href='/student_dash/students_forms/admission_form'
-            className='bg-primary text-white hover:bg-primary/15 hover:text-primary  text-lg py-2 px-6 rounded-full transition-all duration-300'
-          >
-            Admission Form
-          </Link>
+        
         </>
       ) : (
         <div className='relative'>
@@ -244,8 +239,14 @@ useEffect(() => {
           {headerData.map((item, index) => (
             <MobileHeaderLink key={index} item={item} />
           ))}
-          
+            <Link
+            href='/student_dash/students_forms/admission_form'
+            className='bg-primary mt-4 text-white hover:bg-primary/15 hover:text-primary  text-lg py-2 px-6 rounded-full transition-all duration-300'
+          >
+            Admission Form
+          </Link>
         </nav>
+        
       </div>
     </div>
 
