971457143236d7c1aec54705822cb0497f9e1c7b
rvp0001 <34305593+rvp0001@users.noreply.github.com>
2025-03-11 22:20:13 +0530

    update

--- src/app/Students_Management/Students_Management_Page/Id_Card/page.js
+++ src/app/Students_Management/Students_Management_Page/Id_Card/page.js
@@ -140,7 +140,7 @@ const IDCard = () => {
   
 
   return (
-    <div className="flex flex-col items-center mt-10 p-12">
+    <div className="flex flex-col items-center mt-32 p-12">
      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
   {students.map((student, index) => (
     <div key={index} className="relative cursor-pointer" onClick={() => handleSelectCard(index)}>
