bab6d737c2a53d1de9197cfc3eaf2b3c52dd150a
rvp0001 <34305593+rvp0001@users.noreply.github.com>
2025-04-10 12:12:09 +0530

    update

--- src/app/Home/School_Landing_Page/ProfessionalCourses/page.js
+++ src/app/Home/School_Landing_Page/ProfessionalCourses/page.js
@@ -2,12 +2,11 @@
 
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import { GET_CLASS_LIST_DATA } from "../../../query/ClassesDataQuery/fetchClassData";
 import { executeQuery } from "../../../graphqlClient";
-import { Star, Tag, CheckCircle, XCircle, Users, UserCheck, UserMinus } from "lucide-react";
-
-
+import { Star, Tag, Users, UserCheck, UserMinus } from "lucide-react";
+import Loading from "../../../Loader/page";
 
 const heroContent = [
   { image: "/img/schooltwo.jpg", title: "Unlock Your Child’s Potential", description: "Give your child the best start in life with our engaging and interactive classes." },
@@ -20,12 +19,20 @@ export default function ClassSetup() {
   const router = useRouter();
   const [currentIndex, setCurrentIndex] = useState(0);
   const [classes, setClasses] = useState([]);
+  const [isLoading, setIsLoading] = useState(true); 
 
   useEffect(() => {
     const fetchClasses = async () => {
-      const response = await executeQuery(GET_CLASS_LIST_DATA);
-      if (response?.getClassesDataList) {
-        setClasses(response.getClassesDataList);
+      setIsLoading(true); 
+      try {
+        const response = await executeQuery(GET_CLASS_LIST_DATA);
+        if (response?.getClassesDataList) {
+          setClasses(response.getClassesDataList);
+        }
+      } catch (error) {
+        console.error("Error fetching classes:", error);
+      } finally {
+        setIsLoading(false); 
       }
     };
     fetchClasses();
@@ -64,7 +71,7 @@ export default function ClassSetup() {
       case "12th":
         return "/classes/12";
       default:
-        return "/classes/general"; 
+        return "/classes/general";
     }
   };
 
@@ -78,15 +85,21 @@ export default function ClassSetup() {
   const handleIndicatorClick = (index) => {
     setCurrentIndex(index);
   };
-  
-  return (
-    <section className=" bg-gray-100">
 
-<div className="relative mt-4 w-full h-[500px] overflow-hidden">
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  return (
+    <section className="bg-gray-100">
+      <div className="relative mt-4 w-full h-[500px] overflow-hidden">
         {heroContent.map((item, index) => (
           <div
             key={index}
-            className={`absolute inset-0 w-full h-full transition-opacity duration-1000 ${index === currentIndex ? 'opacity-100' : 'opacity-0'}`}
+            className={`absolute inset-0 w-full h-full transition-opacity duration-1000 ${
+              index === currentIndex ? "opacity-100" : "opacity-0"
+            }`}
           >
             <Image src={item.image} layout="fill" objectFit="cover" alt="Hero Image" />
             <div className="absolute inset-0 bg-black bg-opacity-60"></div>
@@ -96,137 +109,133 @@ export default function ClassSetup() {
             </div>
           </div>
         ))}
-        {/* Indicators */}
         <div className="absolute bottom-5 left-1/2 transform -translate-x-1/2 flex gap-2">
           {heroContent.map((_, index) => (
             <span
               key={index}
-              className={`w-3 h-3 rounded-full cursor-pointer ${index === currentIndex ? 'bg-white' : 'bg-gray-400'}`}
+              className={`w-3 h-3 rounded-full cursor-pointer ${
+                index === currentIndex ? "bg-white" : "bg-gray-400"
+              }`}
               onClick={() => handleIndicatorClick(index)}
             ></span>
           ))}
         </div>
       </div>
 
-<div className="max-w-7xl mx-auto px-8 py-10">
+      <div className="max-w-7xl mx-auto px-8 py-10">
         <h2 className="text-3xl lg:text-5xl font-semibold text-blue-900 mb-6 text-center">Classes</h2>
-
         <p className="text-lg text-gray-700 max-w-2xl mx-auto text-center">
-        Find the perfect learning path for each class level.
+          Find the perfect learning path for each class level.
         </p>
- 
 
         <div className="mt-12 grid gap-4 sm:grid-cols-2 lg:grid-cols-3 auto-rows-min">
-        {classes.map((cls, index) => {
-            const availableSeats = cls.total_seats - cls.filled_seats || '0';
-            const classPath = getClassPath(cls.class_title); 
+          {classes.map((cls, index) => {
+            const availableSeats = cls.total_seats - cls.filled_seats || "0";
+            const classPath = getClassPath(cls.class_title);
             return (
-              <div key={index} className="relative bg-white p-6 shadow-lg text-left transition-all duration-500 ease-in-out transform shadow-md scale-100 min-h-[300px] border-b border-gray-300">
+              <div
+                key={index}
+                className="relative bg-white p-6 shadow-lg text-left transition-all duration-500 ease-in-out transform shadow-md scale-100 min-h-[300px] border-b border-gray-300"
+              >
                 <h3 className="text-blue-600 font-semibold text-sm uppercase flex items-center gap-2">
                   <Tag className="w-4 h-4 text-orange-500" /> {cls.class_title}
                 </h3>
                 <p className="text-sm mt-2 text-gray-900">{cls.description}</p>
                 <div className="mt-4 flex flex-wrap gap-2">
-                {cls.tags && typeof cls.tags === "string" 
-  ? cls.tags.split(",").map((tag, idx) => (
-      <span key={idx} className="px-3 py-1 text-sm bg-blue-100 rounded-full flex items-center gap-1">
-        <Tag className="w-3 h-3 text-gray-500" /> {tag.trim()}
-      </span>
-    ))
-  : <p className="text-gray-500 text-sm">No Tags Available</p>
-}
-
+                  {cls.tags && typeof cls.tags === "string" ? (
+                    cls.tags.split(",").map((tag, idx) => (
+                      <span
+                        key={idx}
+                        className="px-3 py-1 text-sm bg-blue-100 rounded-full flex items-center gap-1"
+                      >
+                        <Tag className="w-3 h-3 text-gray-500" /> {tag.trim()}
+                      </span>
+                    ))
+                  ) : (
+                    <p className="text-gray-500 text-sm">No Tags Available</p>
+                  )}
                 </div>
                 <div className="mt-6 flex justify-center">
-  <div className="w-auto h-[275px] overflow-hidden ">
-    <Image
-      src={cls?.image || "/img/1.png"}
-      width={180}
-      height={180}
-      alt={cls?.class_title || "Class Image"}
-      className="w-full h-full object-cover transition-opacity duration-500 ease-in-out hover:opacity-80"
-      unoptimized
-      onError={(e) => (e.target.src = "/window.svg")}
-    />
-  </div>
-</div>
+                  <div className="w-auto h-[275px] overflow-hidden">
+                    <Image
+                      src={cls?.image || "/img/1.png"}
+                      width={180}
+                      height={180}
+                      alt={cls?.class_title || "Class Image"}
+                      className="w-full h-full object-cover transition-opacity duration-500 ease-in-out hover:opacity-80"
+                      unoptimized
+                      onError={(e) => (e.target.src = "/window.svg")}
+                    />
+                  </div>
+                </div>
                 <div className="mt-6 rounded-b-2xl w-full max-w-md mx-auto">
-      {/* Student Rating */}
-      <p
-        className="flex justify-between items-center text-sm border-b border-gray-300 rounded-xl p-3 text-gray-700 cursor-pointer hover:bg-gray-200"
-        onClick={() => router.push('/Home/School_Landing_Page/Students_Review_Rating')}
-      >
-        <span className="flex items-center gap-1">
-          <Star className="w-4 h-4 text-yellow-500" />
-          <span className="font-semibold">Student Rating:</span>
-        </span>
-        <span className="text-green-500 font-semibold">
-          {cls.student_reviews || "NA"}/5 ({cls.student_rating || "NA"})
-        </span>
-      </p>
-
-      {/* Parents Rating */}
-      <p
-        className="flex justify-between items-center text-sm border-b border-gray-300 rounded-xl p-3 text-gray-700 cursor-pointer hover:bg-gray-200 mt-2"
-        onClick={() => router.push('/Home/School_Landing_Page/Parent_Review_Rating')}
-      >
-        <span className="flex items-center gap-1">
-          <Star className="w-4 h-4 text-yellow-500" />
-          <span className="font-semibold">Parents Rating:</span>
-        </span>
-        <span className="text-green-500 font-semibold">
-          {cls.parents_reviews}/5 ({cls.parents_rating})
-        </span>
-      </p>
-
-      {/* Discount */}
-      <p className="flex justify-between items-center text-sm border-b border-gray-300 rounded-xl p-3 text-gray-700 mt-2">
-        <span className="flex items-center gap-1">
-          <Tag className="w-4 h-4 text-red-500" />
-          <span className="font-semibold">Discount:</span>
-        </span>
-        <span className="text-red-500 font-semibold">{cls.discount} % Off</span>
-      </p>
-
-      {/* Seats Info */}
-      <div className="grid grid-cols-1 gap-2 text-sm text-gray-700 mt-2">
-        <p className="flex justify-between items-center border-b border-gray-300 rounded-xl p-3">
-          <span className="flex items-center gap-1">
-            <Users className="w-4 h-4 text-blue-500" />
-            <span className="font-semibold">Total Seats:</span>
-          </span>
-          <span>{cls.total_seats}</span>
-        </p>
-        <p className="flex justify-between items-center border-b border-gray-300 rounded-xl p-3">
-          <span className="flex items-center gap-1">
-            <UserCheck className="w-4 h-4 text-green-500" />
-            <span className="font-semibold">Filled Seats:</span>
-          </span>
-          <span>{cls.filled_seats}</span>
-        </p>
-        <p className="flex justify-between items-center border-b border-gray-300 rounded-xl p-3">
-          <span className="flex items-center gap-1">
-            <UserMinus className="w-4 h-4 text-red-500" />
-            <span className="font-semibold">Available Seats:</span>
-          </span>
-          <span>{availableSeats}</span>
-        </p>
-      </div>
-
-     
-    </div>
-    {availableSeats > 0 ? (
-        <button
-          onClick={() => router.push(classPath)}
-          className="mt-6 w-full bg-gradient-to-r from-green-500 to-green-600 text-white py-3 font-semibold text-lg transition-all duration-500 ease-in-out transform hover:scale-105 hover:from-orange-600 hover:to-orange-700 active:scale-95"
-        >
-         Apply Now
-        </button>
-      ) : (
-        <button className="mt-6 w-full bg-red-600 text-white py-3 font-semibold text-lg" disabled>
-          Admission Closed
-        </button>
-      )}
+                  <p
+                    className="flex justify-between items-center text-sm border-b border-gray-300 rounded-xl p-3 text-gray-700 cursor-pointer hover:bg-gray-200"
+                    onClick={() => router.push("/Home/School_Landing_Page/Students_Review_Rating")}
+                  >
+                    <span className="flex items-center gap-1">
+                      <Star className="w-4 h-4 text-yellow-500" />
+                      <span className="font-semibold">Student Rating:</span>
+                    </span>
+                    <span className="text-green-500 font-semibold">
+                      {cls.student_reviews || "NA"}/5 ({cls.student_rating || "NA"})
+                    </span>
+                  </p>
+                  <p
+                    className="flex justify-between items-center text-sm border-b border-gray-300 rounded-xl p-3 text-gray-700 cursor-pointer hover:bg-gray-200 mt-2"
+                    onClick={() => router.push("/Home/School_Landing_Page/Parent_Review_Rating")}
+                  >
+                    <span className="flex items-center gap-1">
+                      <Star className="w-4 h-4 text-yellow-500" />
+                      <span className="font-semibold">Parents Rating:</span>
+                    </span>
+                    <span className="text-green-500 font-semibold">
+                      {cls.parents_reviews}/5 ({cls.parents_rating})
+                    </span>
+                  </p>
+                  <p className="flex justify-between items-center text-sm border-b border-gray-300 rounded-xl p-3 text-gray-700 mt-2">
+                    <span className="flex items-center gap-1">
+                      <Tag className="w-4 h-4 text-red-500" />
+                      <span className="font-semibold">Discount:</span>
+                    </span>
+                    <span className="text-red-500 font-semibold">{cls.discount} % Off</span>
+                  </p>
+                  <div className="grid grid-cols-1 gap-2 text-sm text-gray-700 mt-2">
+                    <p className="flex justify-between items-center border-b border-gray-300 rounded-xl p-3">
+                      <span className="flex items-center gap-1">
+                        <Users className="w-4 h-4 text-blue-500" />
+                        <span className="font-semibold">Total Seats:</span>
+                      </span>
+                      <span>{cls.total_seats}</span>
+                    </p>
+                    <p className="flex justify-between items-center border-b border-gray-300 rounded-xl p-3">
+                      <span className="flex items-center gap-1">
+                        <UserCheck className="w-4 h-4 text-green-500" />
+                        <span className="font-semibold">Filled Seats:</span>
+                      </span>
+                      <span>{cls.filled_seats}</span>
+                    </p>
+                    <p className="flex justify-between items-center border-b border-gray-300 rounded-xl p-3">
+                      <span className="flex items-center gap-1">
+                        <UserMinus className="w-4 h-4 text-red-500" />
+                        <span className="font-semibold">Available Seats:</span>
+                      </span>
+                      <span>{availableSeats}</span>
+                    </p>
+                  </div>
+                </div>
+                {availableSeats > 0 ? (
+                  <button
+                    onClick={() => router.push(classPath)}
+                    className="mt-6 w-full bg-gradient-to-r from-green-500 to-green-600 text-white py-3 font-semibold text-lg transition-all duration-500 ease-in-out transform hover:scale-105 hover:from-orange-600 hover:to-orange-700 active:scale-95"
+                  >
+                    Apply Now
+                  </button>
+                ) : (
+                  <button className="mt-6 w-full bg-red-600 text-white py-3 font-semibold text-lg" disabled>
+                    Admission Closed
+                  </button>
+                )}
               </div>
             );
           })}
