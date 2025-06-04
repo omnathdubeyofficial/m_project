5c1129e7d93e905202ee37a8aa21bf254e4e0d55
rvp0001 <34305593+rvp0001@users.noreply.github.com>
2025-06-04 23:01:25 +0530

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
-import type { Config } from "tailwindcss";
 
-export default {
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -20,9 +21,45 @@ export default {
         widest: '0.515em',
       },
       borderRadius: {
-        "Newsletter": "30px 400px 30px 30px"
+        Newsletter: "30px 400px 30px 30px",
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
+
+
+
+
+
+
+
+
+// import type { Config } from "tailwindcss";
+
+// export default {
+//   content: [
+//     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         primary: "#df6853",
+//         grey: "#363636",
+//       },
+//       spacing: {
+//         "60%": "60%",
+//         "70%": "70%",
+//       },
+//       letterSpacing: {
+//         widest: '0.515em',
+//       },
+//       borderRadius: {
+//         "Newsletter": "30px 400px 30px 30px"
+//       },
+//     },
+//   },
+//   plugins: [],
+// } satisfies Config;
