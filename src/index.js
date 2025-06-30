f0d2a7cbb66c285fd895a7f318a475a5314e28a7
rvp0001 <omnathdubeyofficial@gmail.com>
2025-06-30 13:18:32 +0530

    update

--- src/index.js
+++ src/index.js
@@ -132,7 +132,7 @@ app.use(
 
 
 // ✅ Start Server
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}/graphql`);
 });
