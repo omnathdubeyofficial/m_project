bc6b046279d41cb4c53e4d8a1b0ad33e89066abd
rvp0001 <omnathdubeyofficial@gmail.com>
2025-06-30 13:49:11 +0530

    update

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+// pages/index.js
+
+export default function HomePage() {
+  return (
+    <div className="text-center mt-10 text-2xl font-bold">
+      Welcome to School Management Software!
+    </div>
+  );
+}
